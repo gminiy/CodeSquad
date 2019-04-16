dbd24ed3bc7903fecbcd92e33a80900d10c9cdcc
Jimin Yeon <gmini.y@gmail.com>
2019-04-16 21:30:24 +0900

    When initialValue is undefined, accumulator is the first element of Array and current value is the second element of Array.

--- JS_COMMON/Step3/Step3-1/step3-1_myReduce.js
+++ JS_COMMON/Step3/Step3-1/step3-1_myReduce.js
@@ -2,6 +2,15 @@ const checkType = require('./module/check_type');
 
 const myReduce = (arr, callback, initialValue) => {
     let accumulator = initialValue;
+
+    if(checkType.isUndefined(initialValue)) {
+        accumulator = arr[0];
+        for(let index = 1; index < arr.length; index++) {
+            accumulator = callback(accumulator, arr[index], index, arr);
+        }
+        return accumulator;
+    }
+
     if(checkType.isArray(accumulator)) {
         for(index in arr) {
             accumulator.push(callback(accumulator, arr[index], index, arr));
@@ -12,9 +21,11 @@ const myReduce = (arr, callback, initialValue) => {
     for(index in arr) {
         accumulator = callback(accumulator, arr[index], index, arr);
     }
+    
     return accumulator;
 }
 
-let testArray = [1,2,3,4,5];
 
-console.log(myReduce(testArray, (accumulator, curVal, curIndex, arr) => curVal + 3, [1,]));
+
+let testArray = [1,2,3,4,5];
+console.log(myReduce(testArray, (accumulator, curVal, curIndex, arr) => accumulator + curVal));
